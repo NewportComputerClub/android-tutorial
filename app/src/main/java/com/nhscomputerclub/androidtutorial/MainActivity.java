30cfbc1b36eb3cbf4281b7660cbbc7db3c0d4459
Terrance Li <terranceli.terbear@gmail.com>
2017-12-05 16:25:11 -0800

    First tutorial

--- app/src/main/java/com/nhscomputerclub/androidtutorial/MainActivity.java
+++ app/src/main/java/com/nhscomputerclub/androidtutorial/MainActivity.java
@@ -2,6 +2,9 @@ package com.nhscomputerclub.androidtutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView helloTextView = findViewById(R.id.hello_textview);
+        final Button clickButton = findViewById(R.id.clickButton);
+
+        helloTextView.setText("Hello Mr. Fincher");
+
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Handle button click
+                clickButton.setText("Clicked");
+            }
+        });
+
+
     }
 }
